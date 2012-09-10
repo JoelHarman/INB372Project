1c54e5bba3eb8221efd5b33b5bea150a21983b01
malwynp <map.viii@gmail.com>
2012-09-10 11:21:35 +1000

    Added an exception class

--- INB372Workspace/Guestbook/src/guestbook/CalculatorException.java
+++ INB372Workspace/Guestbook/src/guestbook/CalculatorException.java
@@ -3,5 +3,9 @@ package guestbook;
 public class CalculatorException extends Exception {
 
 	private static final long serialVersionUID = 1L;
-
+	
+	public CalculatorException(String reason) {
+		super(reason);
+	}
+	
 }
